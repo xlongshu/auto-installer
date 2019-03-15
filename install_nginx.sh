df845383f0a2ded55d7dc7807be214a7622cfd9b
xlongshu <xlongshu@163.com>
2019-03-15 19:42:59 +0800

    update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -34,14 +34,24 @@ function install_nginx() {
 
         cat > /etc/yum.repos.d/nginx.repo <<- EOF
 # nginx.repo
-[nginx]
-name=nginx repo
-baseurl=http://nginx.org/packages/centos/$(centos_version)/\$basearch/
-gpgcheck=0
+
+[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/centos/\$releasever/\$basearch/
+gpgcheck=1
 enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+
+[nginx-mainline]
+name=nginx mainline repo
+baseurl=http://nginx.org/packages/mainline/centos/\$releasever/\$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=https://nginx.org/keys/nginx_signing.key
 
 EOF
 
+        yum install yum-utils
         yum update
         yum install nginx -y
     else
