3bf63c0545fd1ca8fdfd5888f498fa4dd0fd388e
xlongshu <xlongshu@163.com>
2019-03-15 19:44:01 +0800

    update install_hadoop.sh

--- common.sh
+++ common.sh
@@ -1,12 +1,13 @@
 # common.sh
 
 #=================================================
-#	Description: Install common
-#	System Required: CentOS/Debian/Ubuntu
-#	Author: longshu
+#  Description: Common script
+#  System Required: Linux/Unix
+#  Author: LongShu
 #=================================================
 
 
+ADD_APP_USER="false"
 APP_USER=${APP_USER:-"admin"}
 #=================================================
 text_red='\033[0;31m'
@@ -22,20 +23,107 @@ echo "log file ${LOG_FILE}"
 
 function log_info() {
     local date=`date "+%Y-%m-%d %H:%M:%S"`
-    echo -e "${INFO} $*"
+    echo -e "${INFO} $*" 1>&2
     echo "[INFO ][$date]: $*" >> ${LOG_FILE}
 }
 
 function log_warn() {
     local date=`date "+%Y-%m-%d %H:%M:%S"`
-    echo -e "${WARN} $*"
+    echo -e "${WARN} $*" 1>&2
     echo "[WARN][$date]: $*" >> ${LOG_FILE}
 }
 
 function log_err() {
     local date=`date "+%Y-%m-%d %H:%M:%S"`
-    echo -e "${ERROR} $*"
+    echo -e "${ERROR} $*" 1>&2
     echo "[ERROR][$date]: $*" >> ${LOG_FILE}
+}
+
+function replace_str() {
+    local file_path=$(readlink -f "$1")
+    local orig_str=$2
+    local target_str=$3
+    sed -i "s,${orig_str},${target_str}," ${file_path}
+}
+
+function get_path_file_name() {
+    local full_path=$1
+    local file_name="${full_path##*/}"
+    echo ${file_name}
+}
+
+function get_archive_dir_name() {
+    local full_path=$1
+    local archive_name="${full_path##*/}"
+    local dir_name="${archive_name%.tar.gz}"
+    dir_name="${dir_name%.zip}"
+    echo ${dir_name}
+}
+
+function get_name_var() {
+    local full_path=$1
+    local archive_name="${full_path##*/}"
+    # apache-maven-x.y.z-bin.tar.gz, apache-maven-x.y.z-bin.zip
+    local name_ver="${archive_name%.tar.gz}"
+    name_ver="${name_ver%.zip}"
+
+    # apache-maven-x.y.z-bin
+    [[ ${name_ver} == apache-* ]] && name_ver=${name_ver:7}
+    [[ ${name_ver} == *-bin ]] && name_ver=${name_ver:0:-4}
+
+    echo ${name_ver}
+}
+
+# https://stackoverflow.com/questions/592620/how-to-check-if-a-program-exists-from-a-bash-script
+function command_exists() {
+    type "$1" &> /dev/null;
+    # command -v "$1" > /dev/null 2>&1
+}
+
+function download_file() {
+    local dl_url=$1
+    local dl_dir=$(readlink -f "$2")
+
+    local file_name=${3:-"${dl_url##*/}"}
+    local file_path="${dl_dir}/${file_name}"
+
+    if [[ -f "$file_path" ]]; then
+        log_warn "Already exists file [$file_path] !"
+        echo ${file_path}
+        return 0
+    fi
+
+    log_info "Download ${file_name} ..."
+    if command_exists wget; then
+        wget -c -nv -t 2 -T 30 --no-check-certificate -P ${dl_dir} ${dl_url}
+    else
+        curl -L ${dl_url} -o ${file_path}
+    fi
+    echo ${file_path}
+}
+
+function unpack_file() {
+    local file_path=$1
+    local out_dir=$(readlink -f "$2")
+
+    local dir_name=$(get_archive_dir_name ${file_path})
+    local out_path="$(ls -d ${out_dir}/* | grep "${dir_name%-bin}*" | head -1)"
+
+    if [[ -d ${out_path} ]]; then
+        log_warn "Already exists dir [$out_path] !"
+        echo ${out_path}
+        return 0
+    fi
+
+    log_info "Unpacking ${file_path}"
+    if [[ ${file_path} == *.zip ]]; then
+        unzip -q ${file_path} -d ${out_dir}
+    else
+        tar -zxf ${file_path} -C ${out_dir}
+    fi
+
+    out_path=$(ls -d ${out_dir}/* | grep "${dir_name%-bin}*" | head -1)
+    echo ${out_path}
 }
 
 # Make sure only root can run our script, sudo su
@@ -97,8 +185,19 @@ function add_appuser() {
     [[ -n ${username} ]] && APP_USER=${username}
     [[ -z ${password} ]] && password="123456"
 
-    adduser -m -s /bin/bash ${APP_USER}
+    useradd -m -s /bin/bash ${APP_USER}
     echo ${password} | passwd --stdin ${APP_USER}
     log_info "created user: ${APP_USER}:${password}"
 }
 
+function set_owner() {
+    target=$1
+
+    if [[ "ture" == ${ADD_APP_USER} ]]; then
+        owner_user=${APP_USER}
+    else
+        owner_user=$(id -un)
+    fi
+    log_info "Set Owner ${owner_user} -> ${target} ..."
+    chown -R ${owner_user} ${target}
+}
--- install_hadoop.sh
+++ install_hadoop.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+
 # install_hadoop.sh
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 #=================================================
-#	Description: Install Hadoop
-#	System Required: CentOS/Debian/Ubuntu
-#	Author: longshu
+#  Description: Install Hadoop
+#  System Required: Linux/Unix
+#  Author: LongShu
 #=================================================
 
 
@@ -17,8 +18,10 @@ JAVA_VER=8 # 7, 8
 DOWNLOAD_DIR=/data/download
 INSTALL_PREFIX=/opt/app
 DATA_DIR=/data/hadoop
-CONF_DIR=/etc/hadoop
+CONF_DIR=${DATA_DIR}/conf
 LOG_FILE=/tmp/install_hadoop.log
+
+HADOOP_HOST='localhost' # 127.0.0.1, 192.168.56.1
 #=================================================
 
 
@@ -27,14 +30,20 @@ MIRRORS="https://mirrors.tuna.tsinghua.edu.cn/apache"
 # http://mirrors.ustc.edu.cn/apache
 # http://archive.apache.org/dist
 
-ZK_DL_URL="$MIRRORS/zookeeper/zookeeper-3.4.10/zookeeper-3.4.10.tar.gz"
+ZK_DL_URL="$MIRRORS/zookeeper/zookeeper-3.4.13/zookeeper-3.4.13.tar.gz"
 HADOOP_DL_URL="$MIRRORS/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz"
-HBASE_DL_URL="$MIRRORS/hbase/1.2.7/hbase-1.2.7-bin.tar.gz"
-HIVE_DL_URL="$MIRRORS/hive/hive-2.3.3/apache-hive-2.3.3-bin.tar.gz"
+HBASE_DL_URL="$MIRRORS/hbase/1.4.9/hbase-1.4.9-bin.tar.gz"
+HIVE_DL_URL="$MIRRORS/hive/hive-1.2.2/apache-hive-1.2.2-bin.tar.gz"
 
-MYSQL_DRIVER_URL="http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.40/mysql-connector-java-5.1.40.jar"
+# http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.40/mysql-connector-java-5.1.40.jar
+# mysql: com.mysql.jdbc.Driver, jdbc:mysql://localhost:3306/hive?createDatabaseIfNotExist=true&amp;useSSL=false
+# sqlite: org.sqlite.JDBC, jdbc:sqlite:${CONF_DIR}/hive.sqlite
+DRIVER_DL_URL="http://central.maven.org/maven2/org/xerial/sqlite-jdbc/3.15.1/sqlite-jdbc-3.15.1.jar"
 #=================================================
 
+CUR_DIR=$(pwd)
+BASE_DIR="\$(cd "$(dirname "$0")"; pwd)"
+cd ${BASE_DIR}
 
 if [[ -f ./common.sh ]]; then
     echo ". ./common.sh"
@@ -64,47 +73,39 @@ function pre_install() {
 
 function download_install() {
     local dl_url=$1
-    local name_tar_gz="${dl_url##*/}" # hadoop-x.y.z.tar.gz
-    name_ver="${name_tar_gz%%.tar.gz}" # hadoop-x.y.z
-    # hbase-x.y.z-bin
-    if [[ ${name_ver} == hbase* ]]; then
-        name_ver="${name_ver%%-bin}"
-    fi
-    # apache-hive-x.y.z-bin
-    if [[ ${name_ver} == apache-hive* ]]; then
-        target="hive"
+
+    local archive_name=$(get_path_file_name ${dl_url}) # hadoop-x.y.z.tar.gz
+    local name_var=$(get_name_var ${archive_name}) # hadoop-x.y.z
+    local target_path="$INSTALL_PREFIX/$name_var"
+
+    if [[ ! -e ${target_path} ]]; then
+        file_path=$(download_file ${dl_url} ${DOWNLOAD_DIR}) # download_file
+        out_path=$(unpack_file ${file_path} ${INSTALL_PREFIX}) # unpack_file
+        dir_name=$(get_path_file_name ${out_path})
+
+        if [[ ${dir_name} != ${name_var} ]]; then
+            log_info "Rename [${dir_name}] to [${name_var}]."
+            mv ${INSTALL_PREFIX}/${dir_name} ${target_path}
+        fi
+
+        set_owner ${target_path}
+        log_info "Install ${archive_name} into [$target_path]."
     else
-        target="${name_ver%%-*}"
-    fi
-
-    if [[ ! -d ${INSTALL_PREFIX}/${target} ]]; then
-        log_info "download ${dl_url}"
-        [[ ! -f ${DOWNLOAD_DIR}/${name_tar_gz} ]] && wget -c -T45 --no-check-certificate -P ${DOWNLOAD_DIR} ${dl_url}
-
-        log_info "tar -zxf ${name_tar_gz}"
-        tar -zxf ${DOWNLOAD_DIR}/${name_tar_gz} -C ${INSTALL_PREFIX}
-
-        log_info "mv ${name_ver}"
-        mv ${INSTALL_PREFIX}/${name_ver} ${INSTALL_PREFIX}/${target}
-        chgrp -R root ${INSTALL_PREFIX}/${target}
-        chown -R ${APP_USER} ${INSTALL_PREFIX}/${target}
-        log_info "install ${name_ver} into ${INSTALL_PREFIX}/${target}"
-    else
-        log_warn "$INSTALL_PREFIX/$target already exists."
+        log_warn "Already exists [$target_path] !"
     fi
 }
 
-function set_env() {
-    mkdir -p ${DATA_DIR} ${DATA_DIR}/zookeeper ${DATA_DIR}/hbase
-    mkdir -p ${CONF_DIR}/common ${CONF_DIR}/zookeeper ${CONF_DIR}/hbase ${CONF_DIR}/hive
-    chown -R ${APP_USER} ${DATA_DIR}
+function set_hadoop_env() {
+    mkdir -p ${DATA_DIR} ${DATA_DIR}/zookeeper ${DATA_DIR}/hadoop ${DATA_DIR}/hbase
+    mkdir -p ${CONF_DIR}/hadoop ${CONF_DIR}/zookeeper ${CONF_DIR}/hbase ${CONF_DIR}/hive
 
     if [[ -f ${CONF_DIR}/hadoop_env.sh ]]; then
-        log_warn "bak ${CONF_DIR}/hadoop_env.sh"
+        log_warn "Backup [${CONF_DIR}/hadoop_env.sh] !"
         mv -f ${CONF_DIR}/hadoop_env.sh ${CONF_DIR}/hadoop_env.sh.bak
     fi
 
-    cat > ${CONF_DIR}/hadoop_env.sh <<- EOF
+    log_info "Generate [${CONF_DIR}/hadoop_env.sh] ..."
+    cat << EOF > ${CONF_DIR}/hadoop_env.sh
 # hadoop_env.sh
 
 export HADOOP_INSTALL_PREFIX=${INSTALL_PREFIX}
@@ -112,29 +113,30 @@ export HADOOP_DATA_PREFIX=${DATA_DIR}
 export HADOOP_CONF_PREFIX=${CONF_DIR}
 
 # ZooKeeper
-export ZK_HOME=\${HADOOP_INSTALL_PREFIX}/zookeeper
+export ZK_HOME=\${HADOOP_INSTALL_PREFIX}/$(get_name_var ${ZK_DL_URL})
 export ZOOBINDIR=\${ZK_HOME}/bin
 export PATH=\$PATH:\$ZOOBINDIR
 export ZOOCFGDIR=\${HADOOP_CONF_PREFIX}/zookeeper
 export ZOO_LOG_DIR=\${HADOOP_DATA_PREFIX}/zookeeper
 
 # Hadoop
-export HADOOP_HOME=\${HADOOP_INSTALL_PREFIX}/hadoop
+export HADOOP_HOME=\${HADOOP_INSTALL_PREFIX}/$(get_name_var ${HADOOP_DL_URL})
 export HADOOP_PREFIX=\$HADOOP_HOME
 export PATH=\$PATH:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin
 
 export HADOOP_HDFS_HOME=\$HADOOP_HOME
 export HADOOP_YARN_HOME=\$HADOOP_HOME
 
-export HADOOP_CONF_DIR=\${HADOOP_CONF_PREFIX}/common
+export HADOOP_CONF_DIR=\${HADOOP_CONF_PREFIX}/hadoop
 
 # Hbase
-export HBASE_HOME=\${HADOOP_INSTALL_PREFIX}/hbase
+export HBASE_HOME=\${HADOOP_INSTALL_PREFIX}/$(get_name_var ${HBASE_DL_URL})
 export PATH=\$PATH:\$HBASE_HOME/bin
 export HBASE_CONF_DIR=\${HADOOP_CONF_PREFIX}/hbase
+export HBASE_MANAGES_ZK=false
 
 # Hive
-export HIVE_HOME=\${HADOOP_INSTALL_PREFIX}/hive
+export HIVE_HOME=\${HADOOP_INSTALL_PREFIX}/$(get_name_var ${HIVE_DL_URL})
 export PATH=\$PATH:\$HIVE_HOME/bin
 export HIVE_CONF_DIR=\${HADOOP_CONF_PREFIX}/hive
 
@@ -143,8 +145,8 @@ EOF
     chmod +x ${CONF_DIR}/hadoop_env.sh
     . ${CONF_DIR}/hadoop_env.sh
 
-    if [[ -f /home/${APP_USER}/.bashrc ]]; then
-        log_info "add [${CONF_DIR}/hadoop_env.sh] into [/home/${APP_USER}/.bashrc]"
+    if [[ "ture" == ${ADD_APP_USER} && -f /home/${APP_USER}/.bashrc ]]; then
+        log_info "Add [${CONF_DIR}/hadoop_env.sh] into [/home/${APP_USER}/.bashrc]"
         cat >> /home/${APP_USER}/.bashrc <<- EOF
 
 # Hadoop
@@ -153,36 +155,59 @@ if [ -f ${CONF_DIR}/hadoop_env.sh ]; then
 fi
 EOF
     else
-        log_err "not exists /home/${APP_USER}/.bashrc"
+        log_warn "Not exists /home/${APP_USER}/.bashrc"
     fi
 
 }
 
 function config_hadoop() {
-    log_info "cp conf"
-    cp -rf ${HADOOP_HOME}/etc/hadoop/* ${CONF_DIR}/common
+    log_info "Copy conf"
+    # load ${CONF_DIR}/hadoop_env.sh
+    cp -rf ${HADOOP_HOME}/etc/hadoop/* ${CONF_DIR}/hadoop
     cp -rf ${ZK_HOME}/conf/* ${CONF_DIR}/zookeeper
     cp -rf ${HBASE_HOME}/conf/* ${CONF_DIR}/hbase
     cp -rf ${HIVE_HOME}/conf/* ${CONF_DIR}/hive
 
+    cp -rf ${BASE_DIR}/sample_config/hadoop/* ${CONF_DIR}/hadoop
+    cp -rf ${BASE_DIR}/sample_config/hbase/* ${CONF_DIR}/hbase
+    cp -rf ${BASE_DIR}/sample_config/hive/* ${CONF_DIR}/hive
+
+    replace_str ${CONF_DIR}/hadoop/core-site.xml "replace_hadoop_host" "${HADOOP_HOST}"
+    replace_str ${CONF_DIR}/hadoop/core-site.xml "replace_hadoop_tmp_dir" "${DATA_DIR}/hadoop"
+    replace_str ${CONF_DIR}/hadoop/yarn-site.xml "replace_hadoop_host" "${HADOOP_HOST}"
+
+    replace_str ${CONF_DIR}/hbase/hbase-site.xml "replace_hadoop_host" "${HADOOP_HOST}"
+    replace_str ${CONF_DIR}/hbase/hbase-site.xml "replace_hbase_rootdir" "${DATA_DIR}/hbase"
+
+    replace_str ${CONF_DIR}/hbase/hbase-site.xml "replace_hive_jdbc_url" "jdbc:sqlite:${CONF_DIR}/hive.sqlite"
+    replace_str ${CONF_DIR}/hbase/hbase-site.xml "replace_hive_jdbc_driver" "org.sqlite.JDBC"
+    # download jdbc driver
+    jdbc_jar=$(download_file ${DRIVER_DL_URL} "${HBASE_HOME}/lib/")
+    log_info "Driver [$jdbc_jar]"
+
     # zookeeper
-    echo 1 > ${ZOO_LOG_DIR}/myid
+    #echo 1 > ${ZOO_LOG_DIR}/myid
     if [[ -f ${CONF_DIR}/zookeeper/zoo.cfg ]]; then
-        log_warn "bak ${CONF_DIR}/zookeeper/zoo.cfg"
+        log_warn "Backup ${CONF_DIR}/zookeeper/zoo.cfg"
         mv -f ${CONF_DIR}/zookeeper/zoo.cfg ${CONF_DIR}/zookeeper/zoo.cfg.bak
     fi
 
-    cat > ${CONF_DIR}/zookeeper/zoo.cfg <<- EOF
+    cat << EOF > ${CONF_DIR}/zookeeper/zoo.cfg
+
+tickTime=2000
+initLimit=10
+syncLimit=5
 
 clientPort=2181
-dataDir=/data/hadoop/zookeeper
+dataDir=${DATA_DIR}/zookeeper
 
 EOF
 
-    chown -R ${APP_USER} ${CONF_DIR}
+    set_owner ${CONF_DIR}
+    set_owner ${DATA_DIR}
 
 }
 
 #=================================================
-check_sys
-check_root
+#check_sys
+#check_root
